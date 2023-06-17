7afdc049d9c0822462982dcbe9f66318ce770ab5
Damion Silver <drsilver@alumni.uncg.edu>
2023-06-17 18:43:12 -0500

    Swapapi coding challenege completion

---
+++ starwarstest-api/starwarstest-api/Controller/StarWarsController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Domain.Interfaces;
+using System.Threading.Tasks;
+namespace starwarstest_api.controllers{
+
+[ApiController]
+[Route("/")]
+public class StarWarsController : ControllerBase
+{
+    private readonly IStarWarsService _starWarsService;
+
+    public StarWarsController(IStarWarsService starWarsService)
+    {
+        _starWarsService = starWarsService;
+    }
+
+[HttpGet]
+public async Task<IActionResult> GetCharacters()
+{
+    try
+    {
+        var characters = await _starWarsService.GetAllAndSortCharactersAsync();
+        return Ok(characters); //get 200 request
+    }
+    catch (Exception ex)
+    {
+        //this is hwere i would probably log the message as well.
+        // Return a generic error message to the client. I will not expose anything more than needed here.
+        return StatusCode(500, "An error occurred while processing your request. Please try again later.");
+    }
+}
+}
+}
